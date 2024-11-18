c56c0dcdd3ee84c503fb6008307f7832d355c5ef
Ali <balotsgar@gmail.com>
2024-11-18 15:05:40 +0500

    Задача охраны

--- Algorithms labs/Lab2/q5.py
+++ Algorithms labs/Lab2/q5.py
@@ -1,15 +1,21 @@
-sm = 10000
 def get_info(name):
+
     with open(name) as file:
+        slots = [0 for x in range(10000)]
         num = int(file.readline())
         for testN in range(num):
-            s = 0
+            flag = False
             schedule = list(map(int, file.readline().split(' ')))
-            for n in range(1, schedule[0]+1):
-                if s >= sm:
-                    print('Wrong Answer')
-                    break
-                s += schedule[n+1] - schedule[n]
-            print('Accepted')
+            print(schedule)
+            for n in range(1, schedule[0]*2, 2):
+                print(schedule[n], schedule[n+1])
+                for i in range(schedule[n], schedule[n+1]):
+                    if not slots[i]:
+                        slots[i] = 1
+                        flag = True
+            if not flag:
+                print('Wrong')
+            else:
+                print('Accepted')
 
 get_info('input.txt')
