177923981bb2bb5a467d1e72a2b68249356eaa97
Ali <balotsgar@gmail.com>
2024-12-04 18:58:57 +0500

    q2

---
+++ Algorithms labs/Lab4/q2.py
@@ -0,0 +1,26 @@
+cap = [1, 2, 3, 4, 5]
+o = sum(cap)
+cap = sorted(cap)
+
+
+def delimiter(mas):
+    n = list(map(lambda cap: cap * 0.01, mas))
+    print(mas)
+    return sorted([(mas[i] + mas[i - 1] - n[i] - n[i-1]) for i in range(1, len(mas), 2)] + ([mas[-1]] if len(mas) % 2 != 0 else []))
+
+
+def delimiter2(mas):
+    n = list(map(lambda cap: cap * 0.01, mas))
+    t = ([mas[-1]], [])[len(mas) % 2 == 0]
+    mas = (mas, mas[:-1])[len(mas) % 2 != 0]
+    print(mas, t)
+    return sorted([(mas[i] + mas[-i - 1] - n[i] - n[-i-1]) for i in range(len(mas)//2)] + t)
+
+
+while True:
+    cap = delimiter2(cap)
+    if len(cap) == 1:
+        break
+
+print(cap)
+print(o - cap[0])
