3317adbb33a065de5633b863d965014890057be0
Ali <balotsgar@gmail.com>
2024-12-09 10:14:31 +0500

    q5

---
+++ Algorithms labs/Lab4/q5.py
@@ -0,0 +1,12 @@
+p1 = 1/1
+p2 = 1/2
+N = 5
+# x + bx = N
+# (b+1)x = N
+# x = N/(b+1)
+# b = p1/p2 or p2/p1
+# x = p1*time or p2*time
+# time = x/p1 or x/p2
+ex = p1 < p2
+time = [1/p1, 1/p2][ex] + (N - 1)//([p2/p1, p1/p2][ex] + 1) * ([p2/p1, p1/p2][ex] + 1)
+print(time)
